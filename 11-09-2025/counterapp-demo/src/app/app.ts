a12cd19074404916f59711b6a5a8e7a7f1d3f94d
Chetan-6290 <gaikwad12c12@gmail.com>
2025-09-11 19:14:40 +0530

    11/09/2025

---
+++ 11-09-2025/counterapp-demo/src/app/app.ts
@@ -0,0 +1,28 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+title = 'counterapp';
+
+counter = 0;
+
+
+inc(){
+  this.counter++;
+}
+
+dec(){
+  this.counter--;
+}
+
+reset(){
+  this.counter = 0;
+}
+
+}
